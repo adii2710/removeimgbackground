9fe36d402aca74a141eb3db38bee8d4831227d30
adii2710 <adityakhopade2003@gmail.com>
2024-06-24 23:07:56 +0530

    first commit

---
+++ img_croping.py
@@ -0,0 +1,16 @@
+# pip install rembg pillow
+import streamlit as st
+from rembg import remove
+from PIL import Image
+
+# input_path = "images/car10.jpg"
+# output_path = "out.png"
+
+# input = Image.open(input_path)
+# output = remove(input)
+# output.save(output_path)
+side_image1 = st.file_uploader("Upload Right Side Image", type=["jpg", "jpeg", "png"])
+
+side_image1 = Image.open(side_image1)
+output = remove(side_image1)
+st.image(output)
