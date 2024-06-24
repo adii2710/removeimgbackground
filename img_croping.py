fd011ccdfba05979d755917a0723d6755b6f23ae
adii2710 <adityakhopade2003@gmail.com>
2024-06-24 23:20:29 +0530

    first commit

--- img_croping.py
+++ img_croping.py
@@ -11,6 +11,7 @@ from PIL import Image
 # output.save(output_path)
 side_image1 = st.file_uploader("Upload Right Side Image", type=["jpg", "jpeg", "png"])
 
-side_image1 = Image.open(side_image1)
-output = remove(side_image1)
-st.image(output)
+if side_image1:
+    side_image1 = Image.open(side_image1)
+    output = remove(side_image1)
+    st.image(output)
